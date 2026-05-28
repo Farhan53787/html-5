e08a31c73a71425d025e5094d2ff515516e0cc29
Farhan53787 <codgerfarhan@gmail.com>
2026-05-28 21:37:58 +0600

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,8 @@
+Name = "Python";
+Age = 35;
+Popularity = "21%";
+Programming_Language = True;
+print("Name:", Name);
+print("Age:", Age);
+print("Popularity:", Popularity);
+print("It's", Programming_Language, "that Python is a programming language.");
