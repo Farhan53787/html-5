97146ba0e4ba01bd363f5ba2d379062dcdbdd335
Farhan53787 <codgerfarhan@gmail.com>
2026-05-28 22:17:54 +0600

    Add files via upload

---
+++ converstaion.py
@@ -0,0 +1,11 @@
+character1 = "Alice"
+character2 = "Bob"
+spch1 = "Hello, " + character2 + "! How are you?"
+spch2 = "Hi, " + character1 + "! I'm doing well, thanks for asking."
+spch3 = "Oh, your outfit is cool! I like it!"
+spch4 = "Thanks! I just got it yesterday."
+print(character1 + ": " + spch1)
+print(character2 + ": " + spch2)    
+print(character1 + ": " + spch3)
+print(character2 + ": " + spch4)
+    
