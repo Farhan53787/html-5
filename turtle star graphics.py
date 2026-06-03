27705d654fe7483808ba448c29b000229a1d9506
Farhan53787 <codgerfarhan@gmail.com>
2026-06-03 21:55:30 +0600

    Add files via upload

---
+++ turtle star graphics.py
@@ -0,0 +1,33 @@
+import turtle
+
+turtle.Screen().bgcolor("red")
+
+sc = turtle.Screen()
+
+sc.setup(600, 600)
+
+sc.title("Turtle Graphics")
+
+turtle.forward(100)
+
+turtle.left(120)
+turtle.forward(100)
+
+turtle.left(120)
+turtle.forward(100)
+
+turtle.penup()
+turtle.right(150)
+turtle.forward(50)
+
+turtle.pendown()
+turtle.right(90)
+turtle.forward(100)
+
+turtle.right(120)
+turtle.forward(100)
+
+turtle.right(120)
+turtle.forward(100)
+
+turtle.done()
